ab9c16c772438ccd3adba884359cf5df25775b72
sinagod <76248835+sinagod@users.noreply.github.com>
2020-12-20 13:31:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //'use strict';
 exports.main_handler = async (event, context, callback) => {
   try {
-    delete require.cache[require.resolve('./qqreads.js')];
-    require('./qqreads.js')
+    for (const v of event["Message"].split("&")) {
+      console.log(v);
+      var request = require('request');
+      delete require.cache[require.resolve('./'+v+'.js')];
+      require('./'+v+'.js')
+    
+//     delete require.cache[require.resolve('./youth_read_IOS.js')];
+//     require('./youth_read_IOS.js')
   } catch (e) {
     console.error(e)
   }
